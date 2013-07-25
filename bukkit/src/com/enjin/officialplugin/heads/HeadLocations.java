4aa9757db8d9e0c954f6bf82f002e533203abfe3
joshua <joshua@enjin.com>
2013-07-25 17:35:15 +0000

    Fixed debug messages being outputted as a severe error message if it couldn't find the heads.yml file.

--- bukkit/src/com/enjin/officialplugin/heads/HeadLocations.java
+++ bukkit/src/com/enjin/officialplugin/heads/HeadLocations.java
@@ -115,10 +115,8 @@ public class HeadLocations {
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (InvalidConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
