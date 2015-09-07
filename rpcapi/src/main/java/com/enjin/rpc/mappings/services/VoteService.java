cf869e35a124e85c510d2e8a6c6a13ebb5263d23
Evan Lindsay <evanlindsay>
2015-09-07 14:38:32 -0700

    Add Vote service and Votifier.get method

---
+++ rpcapi/src/main/java/com/enjin/rpc/mappings/services/VoteService.java
@@ -0,0 +1,44 @@
+package com.enjin.rpc.mappings.services;
+
+import com.enjin.core.services.Service;
+import com.enjin.rpc.EnjinRPC;
+import com.enjin.rpc.mappings.mappings.general.RPCData;
+import com.google.gson.reflect.TypeToken;
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Request;
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Response;
+import com.thetransactioncompany.jsonrpc2.client.JSONRPC2Session;
+import com.thetransactioncompany.jsonrpc2.client.JSONRPC2SessionException;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class VoteService implements Service {
+    public RPCData<String> get(final String authkey, final Map<String, List<String>> votes) {
+        String method = "Votifier.get";
+        Map<String, Object> parameters = new HashMap<String, Object>() {{
+            put("authkey", authkey);
+            put("votifier", votes);
+        }};
+
+        int id = EnjinRPC.getNextRequestId();
+
+        JSONRPC2Session session = null;
+        JSONRPC2Request request = null;
+        JSONRPC2Response response = null;
+
+        try {
+            session = EnjinRPC.getSession("minecraft.php");
+            request = new JSONRPC2Request(method, parameters, id);
+            response = session.send(request);
+
+            EnjinRPC.debug("JSONRPC2 Request: " + request.toJSONString());
+
+            RPCData<String> data = EnjinRPC.gson.fromJson(response.toJSONString(), new TypeToken<RPCData<String>>() {}.getType());
+            return data;
+        } catch (JSONRPC2SessionException e) {
+            EnjinRPC.debug("Failed Request to " + EnjinRPC.getApiUrl() + ": " + request.toJSONString());
+            return null;
+        }
+    }
+}
