9e4d98ffd525a87830a20c035914c0cde2e499fd
Evan Lindsay <evanlindsay>
2015-10-24 06:17:23 -0700

    Simplify regex of EnjinConsole class

--- bukkit/src/main/java/com/enjin/officialplugin/EnjinConsole.java
+++ bukkit/src/main/java/com/enjin/officialplugin/EnjinConsole.java
@@ -8,22 +8,10 @@ import org.bukkit.ChatColor;
  * Handling misc chat/console functions
  */
 public class EnjinConsole {
-
-    //Chat color codes.
-    protected static Pattern chatColorPattern = Pattern.compile("(?i)&([0-9A-F])");
-    protected static Pattern chatMagicPattern = Pattern.compile("(?i)&([K])");
-    protected static Pattern chatBoldPattern = Pattern.compile("(?i)&([L])");
-    protected static Pattern chatStrikethroughPattern = Pattern.compile("(?i)&([M])");
-    protected static Pattern chatUnderlinePattern = Pattern.compile("(?i)&([N])");
-    protected static Pattern chatItalicPattern = Pattern.compile("(?i)&([O])");
-    protected static Pattern chatResetPattern = Pattern.compile("(?i)&([R])");
+    private static Pattern chatColorPattern = Pattern.compile("(?i)&([0-9A-FK-R])");
 
     public static String[] header() {
-        String[] text = new String[]{
-                ChatColor.GREEN + "=== Enjin Minecraft Plugin ==="
-        };
-
-        return text;
+        return new String[]{ChatColor.GREEN + "=== Enjin Minecraft Plugin ==="};
     }
 
     public static String translateColorCodes(String string) {
@@ -31,14 +19,6 @@ public class EnjinConsole {
             return "";
         }
 
-        String newstring = string;
-        newstring = chatColorPattern.matcher(newstring).replaceAll("\u00A7$1");
-        newstring = chatMagicPattern.matcher(newstring).replaceAll("\u00A7$1");
-        newstring = chatBoldPattern.matcher(newstring).replaceAll("\u00A7$1");
-        newstring = chatStrikethroughPattern.matcher(newstring).replaceAll("\u00A7$1");
-        newstring = chatUnderlinePattern.matcher(newstring).replaceAll("\u00A7$1");
-        newstring = chatItalicPattern.matcher(newstring).replaceAll("\u00A7$1");
-        newstring = chatResetPattern.matcher(newstring).replaceAll("\u00A7$1");
-        return newstring;
+        return chatColorPattern.matcher(string).replaceAll("\u00A7$1");
     }
 }
