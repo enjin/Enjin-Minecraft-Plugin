77646a2fe8d7ef9ddff7ac7895fbd4124cc88469
Evan Lindsay <evanlindsay>
2015-10-25 00:17:53 -0700

    Add purchase endpoint to shop service

--- rpcapi/src/main/java/com/enjin/rpc/mappings/services/ShopService.java
+++ rpcapi/src/main/java/com/enjin/rpc/mappings/services/ShopService.java
@@ -12,10 +12,7 @@ import com.thetransactioncompany.jsonrpc2.JSONRPC2Response;
 import com.thetransactioncompany.jsonrpc2.client.JSONRPC2Session;
 import com.thetransactioncompany.jsonrpc2.client.JSONRPC2SessionException;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ShopService implements Service {
     public RPCData<List<Shop>> get(final String authkey, final String player) {
@@ -105,6 +102,52 @@ public class ShopService implements Service {
             EnjinRPC.debug("JSONRPC2 Response: " + response.toJSONString());
 
             RPCData<List<FilteredItem>> data = EnjinRPC.gson.fromJson(response.toJSONString(), new TypeToken<RPCData<ArrayList<FilteredItem>>>() {}.getType());
+            data.setRequest(request);
+            data.setResponse(response);
+            return data;
+        } catch (JSONRPC2SessionException e) {
+            EnjinRPC.debug(e.getMessage());
+            EnjinRPC.debug("Failed Request to " + session.getURL().toString() + ": " + request.toJSONString());
+            return null;
+        }
+    }
+
+    public RPCData<Integer> purchase(final String authkey, final String player, final int itemId, final Optional<List<String>> variables, final Optional<Integer> customPoints, final Optional<Integer> customPrice, final boolean ignoreMessages) {
+        String method = "Shop.purchase";
+        Map<String, Object> parameters = new HashMap<String, Object>() {{
+            put("authkey", authkey);
+            put("player", player);
+            put("item_id", itemId);
+            put("ignore_messages", ignoreMessages);
+        }};
+
+        if (variables.isPresent()) {
+            parameters.put("variables", variables.get());
+        }
+
+        if (customPoints.isPresent()) {
+            parameters.put("custom_points", customPoints.get());
+        }
+
+        if (customPrice.isPresent()) {
+            parameters.put("custom_price", customPrice.get());
+        }
+
+        int id = EnjinRPC.getNextRequestId();
+
+        JSONRPC2Session session = null;
+        JSONRPC2Request request = null;
+        JSONRPC2Response response = null;
+
+        try {
+            session = EnjinRPC.getSession("minecraft.php");
+            request = new JSONRPC2Request(method, parameters, id);
+            response = session.send(request);
+
+            EnjinRPC.debug("JSONRPC2 Request: " + request.toJSONString());
+            EnjinRPC.debug("JSONRPC2 Response: " + response.toJSONString());
+
+            RPCData<Integer> data = EnjinRPC.gson.fromJson(response.toJSONString(), new TypeToken<RPCData<Integer>>() {}.getType());
             data.setRequest(request);
             data.setResponse(response);
             return data;
