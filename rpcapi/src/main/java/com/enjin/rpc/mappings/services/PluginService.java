ecc1442781de14e6adfaef1c0ef865287b1c4486
Evan Lindsay <evanlindsay>
2015-10-24 23:54:55 -0700

    Add setRank and removeRank to plugin service

--- rpcapi/src/main/java/com/enjin/rpc/mappings/services/PluginService.java
+++ rpcapi/src/main/java/com/enjin/rpc/mappings/services/PluginService.java
@@ -122,4 +122,72 @@ public class PluginService implements Service {
             return null;
         }
     }
+
+    public RPCData<Boolean> setRank(final String authkey, final String player, final String group, final String world) {
+        String method = "Plugin.setRank";
+        Map<String, Object> parameters = new HashMap<String, Object>() {{
+            put("authkey", authkey);
+            put("player", player);
+            put("group", group);
+            put("world", world);
+        }};
+
+        int id = EnjinRPC.getNextRequestId();
+
+        JSONRPC2Session session = null;
+        JSONRPC2Request request = null;
+        JSONRPC2Response response = null;
+
+        try {
+            session = EnjinRPC.getSession("minecraft.php");
+            request = new JSONRPC2Request(method, parameters, id);
+            response = session.send(request);
+
+            EnjinRPC.debug("JSONRPC2 Request: " + request.toJSONString());
+            EnjinRPC.debug("JSONRPC2 Response: " + response.toJSONString());
+
+            RPCData<Boolean> data = EnjinRPC.gson.fromJson(response.toJSONString(), new TypeToken<RPCData<Boolean>>() {}.getType());
+            data.setRequest(request);
+            data.setResponse(response);
+            return data;
+        } catch (JSONRPC2SessionException e) {
+            EnjinRPC.debug(e.getMessage());
+            EnjinRPC.debug("Failed Request to " + session.getURL().toString() + ": " + request.toJSONString());
+            return null;
+        }
+    }
+
+    public RPCData<Boolean> removeRank(final String authkey, final String player, final String group, final String world) {
+        String method = "Plugin.removeRank";
+        Map<String, Object> parameters = new HashMap<String, Object>() {{
+            put("authkey", authkey);
+            put("player", player);
+            put("group", group);
+            put("world", world);
+        }};
+
+        int id = EnjinRPC.getNextRequestId();
+
+        JSONRPC2Session session = null;
+        JSONRPC2Request request = null;
+        JSONRPC2Response response = null;
+
+        try {
+            session = EnjinRPC.getSession("minecraft.php");
+            request = new JSONRPC2Request(method, parameters, id);
+            response = session.send(request);
+
+            EnjinRPC.debug("JSONRPC2 Request: " + request.toJSONString());
+            EnjinRPC.debug("JSONRPC2 Response: " + response.toJSONString());
+
+            RPCData<Boolean> data = EnjinRPC.gson.fromJson(response.toJSONString(), new TypeToken<RPCData<Boolean>>() {}.getType());
+            data.setRequest(request);
+            data.setResponse(response);
+            return data;
+        } catch (JSONRPC2SessionException e) {
+            EnjinRPC.debug(e.getMessage());
+            EnjinRPC.debug("Failed Request to " + session.getURL().toString() + ": " + request.toJSONString());
+            return null;
+        }
+    }
 }
