d371bb83ba99a5ab45b49d16597a70bb8f230a57
Evan Lindsay <evanlindsay>
2015-12-28 16:43:58 -0800

    Update bPermissions listener

--- bukkit/src/main/java/com/enjin/bukkit/listeners/perm/processors/BPermissionsListener.java
+++ bukkit/src/main/java/com/enjin/bukkit/listeners/perm/processors/BPermissionsListener.java
@@ -16,32 +16,6 @@ import java.util.concurrent.ConcurrentHashMap;
 public class BPermissionsListener extends PermissionListener {
     private ConcurrentHashMap<String, String> usereditingwhatplayer = new ConcurrentHashMap<String, String>();
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
-        if (event.isCancelled()) {
-            return;
-        }
-        Player p = event.getPlayer();
-        String command = event.getMessage();
-        //Make sure the user has permissions to run the value, otherwise we are just wasting time...
-        if (p.hasPermission("bPermissions.admin") && command.toLowerCase().startsWith("/user ")) {
-            String[] args = command.split(" ");
-            if (args.length > 1) {
-                if (args.length > 2 && (args[1].trim().equalsIgnoreCase("addgroup") || args[1].trim().equalsIgnoreCase("rmgroup") ||
-                        args[1].trim().equalsIgnoreCase("setgroup"))) {
-                    if (usereditingwhatplayer.containsKey(p.getName())) {
-                        String ep = usereditingwhatplayer.get(p.getName());
-                        OfflinePlayer op = Bukkit.getOfflinePlayer(ep);
-                        //We need to make sure the value executes before we actually grab the data.
-                        update(op);
-                    }
-                } else {
-                    usereditingwhatplayer.put(p.getName(), args[1].trim());
-                }
-            }
-        }
-    }
-
     public void processCommand(CommandSender sender, String command, Cancellable event) {
         if (event.isCancelled()) {
             return;
@@ -50,19 +24,7 @@ public class BPermissionsListener extends PermissionListener {
         //Make sure the user has permissions to run the value, otherwise we are just wasting time...
         if (sender.hasPermission("bPermissions.admin") && command.toLowerCase().startsWith("/user ")) {
             String[] args = command.split(" ");
-            if (args.length > 1) {
-                if (args.length > 2 && (args[1].trim().equalsIgnoreCase("addgroup") || args[1].trim().equalsIgnoreCase("rmgroup") ||
-                        args[1].trim().equalsIgnoreCase("setgroup"))) {
-                    if (usereditingwhatplayer.containsKey(sender.getName())) {
-                        String ep = usereditingwhatplayer.get(sender.getName());
-                        OfflinePlayer op = Bukkit.getOfflinePlayer(ep);
-                        //We need to make sure the value executes before we actually grab the data.
-                        update(op);
-                    }
-                } else {
-                    usereditingwhatplayer.put(sender.getName(), args[1].trim());
-                }
-            }
+            update(sender, args);
         }
     }
 
@@ -74,6 +36,19 @@ public class BPermissionsListener extends PermissionListener {
     public void onPlayerQuit(PlayerQuitEvent e) {
         if (usereditingwhatplayer.containsKey(e.getPlayer().getName())) {
             usereditingwhatplayer.remove(e.getPlayer().getName());
+        }
+    }
+
+    private void update(CommandSender sender, String[] args) {
+        if (args.length > 2 && (args[1].trim().equalsIgnoreCase("addgroup") || args[1].trim().equalsIgnoreCase("rmgroup") || args[1].trim().equalsIgnoreCase("setgroup"))) {
+            if (usereditingwhatplayer.containsKey(sender.getName())) {
+                String ep = usereditingwhatplayer.get(sender.getName());
+                OfflinePlayer op = Bukkit.getOfflinePlayer(ep);
+                //We need to make sure the value executes before we actually grab the data.
+                update(op);
+            }
+        } else if (args.length == 2) {
+            usereditingwhatplayer.put(sender.getName(), args[1].trim());
         }
     }
 }
