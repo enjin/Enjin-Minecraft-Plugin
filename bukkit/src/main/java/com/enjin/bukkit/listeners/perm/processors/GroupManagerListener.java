c3f0cf7505526cb540a3f396f811773df3a41e90
Evan Lindsay <evanlindsay>
2016-01-01 18:15:26 -0800

    Cover a couple instances that GM event isn't triggered

--- bukkit/src/main/java/com/enjin/bukkit/listeners/perm/processors/GroupManagerListener.java
+++ bukkit/src/main/java/com/enjin/bukkit/listeners/perm/processors/GroupManagerListener.java
@@ -1,12 +1,13 @@
 package com.enjin.bukkit.listeners.perm.processors;
 
 import com.enjin.bukkit.listeners.perm.PermissionListener;
+import com.enjin.bukkit.managers.VaultManager;
+import net.milkbowl.vault.permission.Permission;
 import org.anjocaido.groupmanager.events.GMUserEvent;
 import org.anjocaido.groupmanager.events.GMUserEvent.Action;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
-import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -24,6 +25,33 @@ public class GroupManagerListener extends PermissionListener {
 
     @Override
     public void processCommand(CommandSender sender, String command, Event event) {
-        // Not Used
+        String[] parts = command.split(" ");
+        if (parts.length >= 3) {
+            if (parts[0].equalsIgnoreCase("manudel") || parts[0].equalsIgnoreCase("manudelsub")) {
+                if (parts[1].length() > 16 || !VaultManager.isVaultEnabled()) {
+                    return;
+                }
+
+                OfflinePlayer op = Bukkit.getOfflinePlayer(parts[1]);
+                if (op == null) {
+                    return;
+                }
+
+                Permission permission = VaultManager.getPermission();
+                if (permission != null && groupExists(permission, parts[2])) {
+                    update(op);
+                }
+            }
+        }
+    }
+
+    private boolean groupExists(Permission permission, String group) {
+        String[] groups = permission.getGroups();
+        for (String g : groups) {
+            if (group.equalsIgnoreCase(g)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
