565263852fa96495d13c19efebd7cadb5de2bdf1
Evan Lindsay <evanlindsay>
2016-01-01 19:20:28 -0800

    BanListener fixes

--- bukkit/src/main/java/com/enjin/bukkit/tasks/BanLister.java
+++ bukkit/src/main/java/com/enjin/bukkit/tasks/BanLister.java
@@ -1,9 +1,8 @@
 package com.enjin.bukkit.tasks;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import com.enjin.core.Enjin;
 import lombok.Getter;
@@ -16,8 +15,8 @@ public class BanLister implements Runnable {
     @Getter
     private static BanLister instance;
     private EnjinMinecraftPlugin plugin;
-    private Map<String, String> currentbannedplayers = new ConcurrentHashMap<String, String>();
-    private boolean firstrun = true;
+    private List<String> currentBannedPlayers = new CopyOnWriteArrayList<>();
+    private boolean firstRun = true;
 
     public BanLister(EnjinMinecraftPlugin plugin) {
         BanLister.instance = this;
@@ -26,39 +25,36 @@ public class BanLister implements Runnable {
 
     @Override
     public void run() {
-        if (firstrun) {
-            Set<OfflinePlayer> bannedplayerlist = Bukkit.getServer().getBannedPlayers();
+        Set<OfflinePlayer> bannedPlayersList = Bukkit.getServer().getBannedPlayers();
 
-            for (OfflinePlayer player : bannedplayerlist) {
+        if (firstRun) {
+            for (OfflinePlayer player : bannedPlayersList) {
                 if (player != null && player.getName() != null) {
-                    currentbannedplayers.put(player.getUniqueId().toString(), "");
+                    currentBannedPlayers.add(player.getUniqueId().toString());
                 }
             }
 
-            firstrun = false;
+            firstRun = false;
         } else {
             Enjin.getPlugin().debug("Scanning banned player list");
-            Set<OfflinePlayer> bannedplayerlist = Bukkit.getServer().getBannedPlayers();
-            HashMap<String, String> lowercasebans = new HashMap<String, String>();
+            List<String> lowercaseBans = new ArrayList<>();
 
             //Checking for bans being added by console or plugin
-            for (OfflinePlayer player : bannedplayerlist) {
+            for (OfflinePlayer player : bannedPlayersList) {
                 if (player != null && player.getName() != null) {
-                    lowercasebans.put(player.getName().toLowerCase(), "");
-                    if (!currentbannedplayers.containsKey(player.getName().toLowerCase())) {
-                        currentbannedplayers.put(player.getName().toLowerCase(), "");
+                    lowercaseBans.add(player.getName().toLowerCase());
+                    if (!currentBannedPlayers.contains(player.getName().toLowerCase())) {
+                        currentBannedPlayers.add(player.getName().toLowerCase());
                         plugin.getBannedPlayers().put(player.getName().toLowerCase(), "");
                         Enjin.getPlugin().debug("Adding banned player " + player.getName());
                     }
                 }
             }
 
-            //checking for pardons being done by console or plugin
-            Set<String> keys = currentbannedplayers.keySet();
-            for (String player : keys) {
-                if (!lowercasebans.containsKey(player)) {
-                    currentbannedplayers.remove(player);
-                    plugin.getPardonedPlayers().put(player, "");
+            for (String player : new ArrayList<>(currentBannedPlayers)) {
+                if (!lowercaseBans.contains(player.toLowerCase())) {
+                    currentBannedPlayers.remove(player.toLowerCase());
+                    plugin.getPardonedPlayers().put(player.toLowerCase(), "");
                     Enjin.getPlugin().debug(player + " was pardoned. Adding to pardoned list.");
                 }
             }
@@ -68,32 +64,27 @@ public class BanLister implements Runnable {
     /**
      * Add a banned player to the list without adding them to the banned players
      * in the plugin
-     *
-     * @param name Name of the player that got banned.
      */
-    public synchronized void addBannedPlayer(OfflinePlayer name) {
-        currentbannedplayers.put(name.getUniqueId().toString(), "");
+    public synchronized void addBannedPlayer(OfflinePlayer op) {
+        currentBannedPlayers.add(op.getName().toLowerCase());
     }
 
 
     /**
      * Remove a banned player to the list without adding them to the pardoned players
      * in the plugin
-     *
-     * @param name Name of the player that got pardoned.
      */
-    public synchronized void pardonBannedPlayer(OfflinePlayer name) {
-        currentbannedplayers.remove(name.getUniqueId().toString());
+    public synchronized void pardonBannedPlayer(OfflinePlayer op) {
+        currentBannedPlayers.remove(op.getName().toLowerCase());
     }
 
     /**
      * Is the player in the ban list?
      *
-     * @param name Name of the player to check
      * @return true if the player is banned, false otherwise.
      */
-    public synchronized boolean playerIsBanned(OfflinePlayer name) {
-        return currentbannedplayers.containsKey(name.getUniqueId().toString());
+    public synchronized boolean playerIsBanned(OfflinePlayer op) {
+        return currentBannedPlayers.contains(op.getName().toLowerCase());
     }
 
 }
