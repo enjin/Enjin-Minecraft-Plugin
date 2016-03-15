066cc7cafb636599a557897f9ff36dc8ea593719
Evan Lindsay <evanlindsay@live.com>
2016-03-14 21:22:38 -0700

    Update help listing

--- sponge/src/main/java/com/enjin/sponge/command/commands/CoreCommands.java
+++ sponge/src/main/java/com/enjin/sponge/command/commands/CoreCommands.java
@@ -23,7 +23,6 @@ import org.spongepowered.api.plugin.PluginContainer;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.channel.MessageChannel;
 import org.spongepowered.api.text.format.TextColors;
-import org.spongepowered.api.text.format.TextStyles;
 import org.spongepowered.api.world.World;
 
 import java.text.DateFormat;
@@ -45,9 +44,9 @@ public class CoreCommands {
             sender.sendMessage(Text.of(TextColors.GOLD, "/enjin broadcast <MESSAGE>: ", TextColors.RESET, "Broadcast a message to all players."));
         }
 
-        if (sender.hasPermission("enjin.push")) {
-            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin push: ", TextColors.RESET, "Sync your website tags with the current ranks."));
-        }
+//        if (sender.hasPermission("enjin.push")) {
+//            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin push: ", TextColors.RESET, "Sync your website tags with the current ranks."));
+//        }
 
         if (sender.hasPermission("enjin.lag")) {
             sender.sendMessage(Text.of(TextColors.GOLD, "/enjin lag: ", TextColors.RESET, "Display TPS average and memory usage."));
@@ -61,59 +60,60 @@ public class CoreCommands {
             sender.sendMessage(Text.of(TextColors.GOLD, "/enjin report: ", TextColors.RESET, "Generate a report file that you can send to Enjin Support for troubleshooting."));
         }
 
-        if (sender.hasPermission("enjin.sign.set")) {
-            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin heads: ", TextColors.RESET, "Shows in game help for the heads and sign stats part of the plugin."));
-        }
-
+//        if (sender.hasPermission("enjin.sign.set")) {
+//            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin heads: ", TextColors.RESET, "Shows in game help for the heads and sign stats part of the plugin."));
+//        }
+//
         if (sender.hasPermission("enjin.tags.view")) {
             sender.sendMessage(Text.of(TextColors.GOLD, "/enjin tags <player>: ", TextColors.RESET, "Shows the tags on the website for the player."));
         }
 
         // Points commands
-        if (sender.hasPermission("enjin.points.getself")) {
-            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin points: ", TextColors.RESET, "Shows your current website points."));
-        }
-
-        if (sender.hasPermission("enjin.points.getothers")) {
-            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin points <NAME>: ", TextColors.RESET, "Shows another player's current website points."));
-        }
-
-        if (sender.hasPermission("enjin.points.add")) {
-            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin addpoints <NAME> <AMOUNT>: ", TextColors.RESET, "Add points to a player."));
-        }
-
-        if (sender.hasPermission("enjin.points.remove")) {
-            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin removepoints <NAME> <AMOUNT>: ", TextColors.RESET, "Remove points from a player."));
-        }
-
-        if (sender.hasPermission("enjin.points.set")) {
-            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin setpoints <NAME> <AMOUNT>: ", TextColors.RESET, "Set a player's total points."));
-        }
-
-        if (sender.hasPermission("enjin.support")) {
-            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin support: ", TextColors.RESET, "Starts ticket session or informs player of available modules."));
-        }
-
-        if (sender.hasPermission("enjin.ticket.self")) {
-            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin ticket: ", TextColors.RESET, "Sends player a list of their tickets."));
-        }
-
-        if (sender.hasPermission("enjin.ticket.open")) {
-            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin openticket: ", TextColors.RESET, "Sends player a list of open tickets."));
-        }
-
-        if (sender.hasPermission("enjin.ticket.reply")) {
-            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin reply <module #> <ticket id> <message>: ", TextColors.RESET, "Sends a reply to a ticket."));
-        }
-
-        if (sender.hasPermission("enjin.ticket.status")) {
-            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin ticketstatus <module #> <ticket id> <open|pending|closed>: ", TextColors.RESET, "Sets the status of a ticket."));
-        }
+//        if (sender.hasPermission("enjin.points.getself")) {
+//            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin points: ", TextColors.RESET, "Shows your current website points."));
+//        }
+//
+//        if (sender.hasPermission("enjin.points.getothers")) {
+//            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin points <NAME>: ", TextColors.RESET, "Shows another player's current website points."));
+//        }
+//
+//        if (sender.hasPermission("enjin.points.add")) {
+//            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin addpoints <NAME> <AMOUNT>: ", TextColors.RESET, "Add points to a player."));
+//        }
+//
+//        if (sender.hasPermission("enjin.points.remove")) {
+//            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin removepoints <NAME> <AMOUNT>: ", TextColors.RESET, "Remove points from a player."));
+//        }
+//
+//        if (sender.hasPermission("enjin.points.set")) {
+//            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin setpoints <NAME> <AMOUNT>: ", TextColors.RESET, "Set a player's total points."));
+//        }
+//
+//        if (sender.hasPermission("enjin.support")) {
+//            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin support: ", TextColors.RESET, "Starts ticket session or informs player of available modules."));
+//        }
+//
+//        if (sender.hasPermission("enjin.ticket.self")) {
+//            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin ticket: ", TextColors.RESET, "Sends player a list of their tickets."));
+//        }
+//
+//        if (sender.hasPermission("enjin.ticket.open")) {
+//            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin openticket: ", TextColors.RESET, "Sends player a list of open tickets."));
+//        }
+//
+//        if (sender.hasPermission("enjin.ticket.reply")) {
+//            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin reply <module #> <ticket id> <message>: ", TextColors.RESET, "Sends a reply to a ticket."));
+//        }
+//
+//        if (sender.hasPermission("enjin.ticket.status")) {
+//            sender.sendMessage(Text.of(TextColors.GOLD, "/enjin ticketstatus <module #> <ticket id> <open|pending|closed>: ", TextColors.RESET, "Sets the status of a ticket."));
+//        }
 
         // Shop buy commands
         sender.sendMessage(Text.of(TextColors.GOLD, "/buy: ", TextColors.RESET, "Display items available for purchase."));
         sender.sendMessage(Text.of(TextColors.GOLD, "/buy page <#>: ", TextColors.RESET, "View the next page of results."));
         sender.sendMessage(Text.of(TextColors.GOLD, "/buy <ID>: ", TextColors.RESET, "Purchase the specified item ID in the server shop."));
+		sender.sendMessage(Text.of(TextColors.GOLD, "/buy shop [ID] ", TextColors.RESET, "Shows the shop menu or opens the shop with the specified ID."));
     }
 
 	@Permission(value = "enjin.broadcast")
