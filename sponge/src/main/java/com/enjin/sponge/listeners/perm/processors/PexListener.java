c5fd113191a031ff369ead202932eb6127f1fda1
Evan Lindsay <evanlindsay@live.com>
2016-03-17 15:22:12 -0700

    [Sponge] Update pex command regex.

--- sponge/src/main/java/com/enjin/sponge/listeners/perm/processors/PexListener.java
+++ sponge/src/main/java/com/enjin/sponge/listeners/perm/processors/PexListener.java
@@ -8,7 +8,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PexListener extends PermissionListener {
-	private static final Pattern PEX_USER_PARENT_ADD_GROUP = Pattern.compile("^(?:pex|permissionsex|permissions) user ([a-zA-Z0-9_]{2,16}) (?:parent|parents) (?:add|a|\\+|remove|rem) group (?:[a-zA-Z0-9]{1,32})$");
+	private static final Pattern PEX_USER_PARENT_ADD_GROUP = Pattern.compile("^(?:pex|permissionsex|permissions) user "
+			+ "([a-zA-Z0-9_]{2,16}) (?:parents|parent|par|p) (?:add|a|\\+|remove|rem|delete|del) "
+			+ "group (?:[a-zA-Z0-9]{1,32})$");
 	private static final Pattern PROMOTE_DEMOTE_USER = Pattern.compile("^(?:promote|prom|demote|dem) user ([a-zA-Z0-9_]{2,16})$");
 
     public void processCommand(CommandSource sender, String command, SendCommandEvent event) {
