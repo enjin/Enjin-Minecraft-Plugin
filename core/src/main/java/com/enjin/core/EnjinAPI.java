b60c9354cb968fe5cc62b280a239d1804b6ee6eb
Evan Lindsay <evanlindsay@live.com>
2016-04-03 05:16:01 -0700

    Need to initialize this.

--- core/src/main/java/com/enjin/core/EnjinAPI.java
+++ core/src/main/java/com/enjin/core/EnjinAPI.java
@@ -1,12 +1,13 @@
 package com.enjin.core;
 
 import com.google.common.base.Predicate;
+import com.google.common.collect.Lists;
 
 import java.util.List;
 import java.util.UUID;
 
 public class EnjinAPI {
-	private List<Predicate<UUID>> vanishRegistrations;
+	private List<Predicate<UUID>> vanishRegistrations = Lists.newArrayList();
 
 	public void registerVanishPredicate(Predicate<UUID> predicate) {
 		if (predicate == null) {
