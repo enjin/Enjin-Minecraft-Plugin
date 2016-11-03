8303b37da880738dad08b9d9d3f23f281c52cb7a
Evan Lindsay <evanlindsay@live.com>
2016-11-03 14:37:28 -0700

    Support latest versions of PermissionManager

--- sponge/src/main/java/com/enjin/sponge/permissions/handlers/PermissionManagerHandler.java
+++ sponge/src/main/java/com/enjin/sponge/permissions/handlers/PermissionManagerHandler.java
@@ -1,15 +1,26 @@
 package com.enjin.sponge.permissions.handlers;
 
+import com.enjin.sponge.listeners.ConnectionListener;
 import com.enjin.sponge.permissions.PermissionHandler;
-import io.github.djxy.permissionManager.PMService;
-import io.github.djxy.permissionManager.PermissionManager;
-import io.github.djxy.permissionManager.subjects.Group;
-import io.github.djxy.permissionManager.subjects.Subject;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import io.github.djxy.permissionmanager.subjects.Permission;
+import io.github.djxy.permissionmanager.subjects.SubjectData;
+import io.github.djxy.permissionmanager.subjects.SubjectDataListener;
+import io.github.djxy.permissionmanager.subjects.group.Group;
+import io.github.djxy.permissionmanager.subjects.group.GroupCollection;
+import io.github.djxy.permissionmanager.subjects.user.User;
+import io.github.djxy.permissionmanager.subjects.user.UserCollection;
+import java.util.Set;
+import java.util.UUID;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.profile.GameProfile;
+import org.spongepowered.api.service.context.Context;
 import org.spongepowered.api.service.permission.PermissionService;
+import org.spongepowered.api.service.permission.Subject;
 import org.spongepowered.api.world.World;
 
 import java.util.HashMap;
@@ -19,84 +30,100 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class PermissionManagerHandler implements PermissionHandler {
-    private PMService service;
-
-    public PermissionManagerHandler(PermissionService service) {
-        if (!(service instanceof PMService)) {
-            throw new IllegalArgumentException("PermissionService is not instance of PMService");
+    private SubjectDataListener listener = new SubjectDataListener() {
+        @Override
+        public void onSetPermission(Set<Context> set, Subject subject, Permission permission) {
+            //
         }
 
-        this.service = (PMService) service;
+        @Override
+        public void onRemovePermission(Set<Context> set, Subject subject, String s) {
+            //
+        }
+
+        @Override
+        public void onAddGroup(Set<Context> set, Subject subject, Group group) {
+            Optional<Player> player = Sponge.getServer().getPlayer(UUID.fromString(subject.getIdentifier()));
+            if (player.isPresent()) ConnectionListener.updatePlayerRanks(player.get());
+        }
+
+        @Override
+        public void onRemoveGroup(Set<Context> set, Subject subject, Group group) {
+            Optional<Player> player = Sponge.getServer().getPlayer(UUID.fromString(subject.getIdentifier()));
+            if (player.isPresent()) ConnectionListener.updatePlayerRanks(player.get());
+        }
+    };
+
+    public PermissionManagerHandler(PermissionService service) {
+        if (!(service instanceof io.github.djxy.permissionmanager.PermissionService)) {
+            throw new IllegalArgumentException("PermissionService is not instance of PMService");
+        }
     }
 
     @Override
     public void onJoin(ClientConnectionEvent.Join event) {
+        User user = (User) UserCollection.instance.get(event.getTargetEntity().getUniqueId().toString());
+        user.getSubjectData().addListener(listener);
     }
 
     @Override
     public void onDisconnect(ClientConnectionEvent.Disconnect event) {
+        User user = (User) UserCollection.instance.get(event.getTargetEntity().getUniqueId().toString());
+        user.getSubjectData().removeListener(listener);
     }
 
     @Override
     public Map<String, List<String>> fetchPlayerGroups(GameProfile player) {
-        PermissionManager manager = PermissionManager.getInstance();
-        io.github.djxy.permissionManager.subjects.Player pp = manager.getOrCreatePlayer(player.getUniqueId());
-
         Map<String, List<String>> worlds = new HashMap<>();
-        if (pp != null) {
-            List<Group> global = pp.getGlobalGroups();
-            if (global != null && !global.isEmpty()) {
-                worlds.put("*", global.stream().map(Subject::getIdentifier).collect(Collectors.toList()));
-            }
+        User user = (User) UserCollection.instance.get(player.getUniqueId().toString());
 
-            for (World world : Sponge.getServer().getWorlds()) {
-                List<Group> groups = pp.getWorldGroups(world.getName());
-                if (groups != null && !groups.isEmpty()) {
-                    worlds.put(world.getName(), groups.stream().map(Subject::getIdentifier).collect(Collectors.toList()));
-                }
-            }
-        }
+        worlds.put("*", user.getParents(SubjectData.GLOBAL_CONTEXT).stream().map(Subject::getIdentifier).collect(Collectors.toList()));
+
+        for (World world : Sponge.getServer().getWorlds())
+            worlds.put(world.getName(), user.getParents(Sets.newHashSet(new Context(Context.WORLD_KEY, world.getName()))).stream()
+                    .map(Subject::getIdentifier)
+                    .collect(Collectors.toList()));
 
         return worlds;
     }
 
     @Override
     public List<String> fetchGroups() {
-        PermissionManager manager = PermissionManager.getInstance();
-        return manager.getGroups().stream().map(Subject::getIdentifier).collect(Collectors.toList());
+        List<String> groups = Lists.newArrayList();
+
+        for (Subject subject : GroupCollection.instance.getAllSubjects())
+            groups.add(subject.getIdentifier());
+
+        return groups;
     }
 
     @Override
     public void addGroup(String player, String group, String world) {
         Optional<Player> optional = Sponge.getServer().getPlayer(player);
-        if (optional.isPresent()) {
-            Player p = optional.get();
-            PermissionManager manager = PermissionManager.getInstance();
+        if (!optional.isPresent()) return;
 
-            Group g = manager.getGroup(group);
-            if (g != null) {
-                io.github.djxy.permissionManager.subjects.Player pp = manager.getOrCreatePlayer(p.getUniqueId());
-                if (pp != null) {
-                    pp.addGroup(g);
-                }
-            }
-        }
+        User user = (User) UserCollection.instance.get(optional.get().getUniqueId().toString());
+        Subject subjectGroup = GroupCollection.instance.get(group);
+        if (subjectGroup == null) return;
+
+        if (world.equals("*"))
+            user.getSubjectData().addParent(SubjectData.GLOBAL_CONTEXT, subjectGroup);
+        else
+            user.getSubjectData().addParent(Sets.newHashSet(new Context(Context.WORLD_KEY, world)), subjectGroup);
     }
 
     @Override
     public void removeGroup(String player, String group, String world) {
         Optional<Player> optional = Sponge.getServer().getPlayer(player);
-        if (optional.isPresent()) {
-            Player p = optional.get();
-            PermissionManager manager = PermissionManager.getInstance();
+        if (!optional.isPresent()) return;
 
-            Group g = manager.getGroup(group);
-            if (g != null) {
-                io.github.djxy.permissionManager.subjects.Player pp = manager.getOrCreatePlayer(p.getUniqueId());
-                if (pp != null) {
-                    pp.removeGroup(g);
-                }
-            }
-        }
+        User user = (User) UserCollection.instance.get(optional.get().getUniqueId().toString());
+        Subject subjectGroup = GroupCollection.instance.get(group);
+        if (subjectGroup == null) return;
+
+        if (world.equals("*"))
+            user.getSubjectData().removeParent(SubjectData.GLOBAL_CONTEXT, subjectGroup);
+        else
+            user.getSubjectData().removeParent(Sets.newHashSet(new Context(Context.WORLD_KEY, world)), subjectGroup);
     }
 }
