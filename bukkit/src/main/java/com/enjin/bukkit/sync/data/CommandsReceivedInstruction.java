5e8a74cf9c1fe244d977c69eb86f01b2b72ad3a8
Evan Lindsay <evanlindsay@live.com>
2018-09-24 15:47:59 -0700

    Remove any null values from the confirmed list

--- bukkit/src/main/java/com/enjin/bukkit/sync/data/CommandsReceivedInstruction.java
+++ bukkit/src/main/java/com/enjin/bukkit/sync/data/CommandsReceivedInstruction.java
@@ -1,15 +1,21 @@
 package com.enjin.bukkit.sync.data;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 import com.enjin.core.Enjin;
 
 public class CommandsReceivedInstruction {
+
+    private static final Collection NULL_SINGLETON_COLLECTION = Collections.singleton(null);
+
     public static void handle(ArrayList<Long> confirmed) {
         if (confirmed == null) {
             return;
         }
 
+        confirmed.removeAll(NULL_SINGLETON_COLLECTION);
         Enjin.getPlugin().getInstructionHandler().commandConfirmed(confirmed);
     }
 }
--- sponge6/src/main/java/com/enjin/sponge/sync/data/CommandsReceivedInstruction.java
+++ sponge6/src/main/java/com/enjin/sponge/sync/data/CommandsReceivedInstruction.java
@@ -3,13 +3,19 @@ package com.enjin.sponge.sync.data;
 import com.enjin.core.Enjin;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class CommandsReceivedInstruction {
+
+    private static final Collection NULL_SINGLETON_COLLECTION = Collections.singleton(null);
+
     public static void handle(ArrayList<Long> confirmed) {
         if (confirmed == null) {
             return;
         }
 
+        confirmed.removeAll(NULL_SINGLETON_COLLECTION);
         Enjin.getPlugin().getInstructionHandler().commandConfirmed(confirmed);
     }
 }
