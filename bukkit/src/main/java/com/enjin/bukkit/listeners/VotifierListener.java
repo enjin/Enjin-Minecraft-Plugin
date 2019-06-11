12277153eb676df2bf7948488db349f357167328
Evan Lindsay <evanlindsay@live.com>
2019-06-10 20:28:08 -0700

    Fetch uuid of voter async

--- bukkit/src/main/java/com/enjin/bukkit/listeners/VotifierListener.java
+++ bukkit/src/main/java/com/enjin/bukkit/listeners/VotifierListener.java
@@ -2,6 +2,7 @@ package com.enjin.bukkit.listeners;
 
 import com.enjin.bukkit.EnjinMinecraftPlugin;
 import com.enjin.bukkit.modules.impl.VotifierModule;
+import com.enjin.bukkit.util.UUIDFetcher;
 import com.enjin.core.Enjin;
 import com.enjin.core.util.Timer;
 import com.vexsoftware.votifier.model.Vote;
@@ -13,6 +14,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 import java.util.ArrayList;
+import java.util.UUID;
 import java.util.regex.Pattern;
 
 public class VotifierListener implements Listener {
@@ -25,23 +27,30 @@ public class VotifierListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void voteRecieved(VotifierEvent event) {
-        Vote vote = event.getVote();
-        String username = vote.getUsername().replaceAll("[^0-9A-Za-z_]", "");
+        final Vote vote = event.getVote();
+        final String username = vote.getUsername().replaceAll("[^0-9A-Za-z_]", "");
+        final String listName = event.getVote().getServiceName().replaceAll("[^0-9A-Za-z.\\-]", "");
+        final VotifierModule module = plugin.getModuleManager().getModule(VotifierModule.class);
 
-        if (username.isEmpty()) return;
+        if (username.isEmpty() || listName.isEmpty()) return;
 
-        String userid = username;
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+            String userid = username;
 
-        if (Bukkit.getOnlineMode()) {
-            OfflinePlayer op = Bukkit.getOfflinePlayer(username);
-            userid = username + "|" + op.getUniqueId().toString();
-        }
+            if (Bukkit.getOnlineMode()) {
+                try {
+                    UUID uuid = UUIDFetcher.getUUIDOf(userid);
+                    if (uuid != null) userid = username + "|" + uuid.toString();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
 
-        String listName = event.getVote().getServiceName().replaceAll("[^0-9A-Za-z.\\-]", "");
-        VotifierModule module = plugin.getModuleManager().getModule(VotifierModule.class);
-        if (!module.getPlayerVotes().containsKey(listName)) {
-            module.getPlayerVotes().put(listName, new ArrayList<>());
-        }
-        module.getPlayerVotes().get(listName).add(new Object[] {userid, System.currentTimeMillis() / 1000});
+            if (!module.getPlayerVotes().containsKey(listName)) {
+                module.getPlayerVotes().put(listName, new ArrayList<>());
+            }
+
+            module.getPlayerVotes().get(listName).add(new Object[]{userid, System.currentTimeMillis() / 1000});
+        });
     }
 }
